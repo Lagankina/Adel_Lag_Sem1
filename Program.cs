9222bcbe1c7f4d86fbf29f920c4e4d302b65d9b4
Lagankina <reirianet@mail.ru>
2023-02-23 22:36:23 +0300

    Задача1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу котораяд принимает два числа  и проверяет является ли первое число квадратом второг
+int a;
+int b;
+Console.WriteLine ("Введите число а: ");
+a = int.Parse(Console.ReadLine());
+ Console.WriteLine ("Введите число b: ");
+ b = int.Parse(Console.ReadLine());
+
+ if (a*a == b) {
+    Console.WriteLine("ДА");
+ }
+ else {
+    Console.WriteLine ("НЕТ");
+ }
